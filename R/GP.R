63cf47dafb15a5b6609f10a6883a7a1664a884a4
Trent Henderson <trent.henderson1@outlook.com>
2024-05-04 22:22:56 +1000

    core functionality works

--- R/GP.R
+++ R/GP.R
@@ -3,26 +3,34 @@
 #' @param x \code{numeric} vector of input data
 #' @param xprime \code{numeric} vector of data points to predict
 #' @param y \code{numeric} vector values to learn from
-#' @param Sigma \code{matrix} containing the covariance matrix
+#' @param covfun \code{function} specifying the covariance function to use
 #' @return \code{TSGP} object containing the input data, posterior mean function and covariance matrix
 #' @author Trent Henderson
 #' @export
 #' @examples
 #' x1 <- 1:100
 #' y <- 3 * sin(2 * seq(0, 4 * pi, length.out = 100)) + runif(100) * 2 + (0.08 * seq(from = 1, to = 100, by = 1))
-#' Sigma <- cov_exp_quad(x1, x1, 0.05, 1)
-#' mod <- GP(x1)
+#'
+#' CovSum <- function(xa, xb, sigma_1 = 1, sigma_2 = 1, l_1 = 1, l_2 = 1, p = 1){
+#'   Sigma_exp_quad <- cov_exp_quad(xa, xb, sigma_1, l_1)
+#'   Sigma_periodic <- cov_periodic(xa, xb, sigma_2, l_2, p)
+#'   Sigma_noise <- cov_noise(sigma_3, nrow(Sigma_exp_quad))
+#'   Sigma <- Sigma_exp_quad + Sigma_periodic
+#'   return(Sigma)
+#' }
+#'
+#' mod <- GP(x1, 1:length(y), y, CovSum, 0.5)
 #'
 
-GP <- function(x, xprime, y, Sigma){
-  Sigma_11 <- cov_function(x, x, ...)
-  Sigma_12 <- cov_function(x, xprime, ...)
+GP <- function(x, xprime, y, covfun, sigma_noise = 0.5, ...){
+  Sigma_11 <- covfun(x, x, ...) + diag(sigma_noise ^ 2, length(x))
+  Sigma_12 <- covfun(x, xprime, ...)
   Sigma_inv <- t(solve(Sigma_11, Sigma_12))
-  Sigma_22 <- cov_function(xprime, xprime, ...)
+  Sigma_22 <- covfun(xprime, xprime, ...)
   Sigma_2 <- Sigma_22 - (Sigma_inv %*% Sigma_12) # Posterior covariance matrix
   mu_2 <- Sigma_inv %*% y # Posterior mean vector
   gp <- list(x, xprime, y, mu_2, Sigma_2)
   names(gp) <- c("x", "xprime", "y", "mu", "Sigma")
-  gp <- structure(gp, class = c("TSGP", "list"))
+  gp <- structure(gp, class = c("GaussProc", "list"))
   return(gp)
 }
--- R/plots.R
+++ R/plots.R
@@ -42,11 +42,11 @@ plot.GPCov <- function(x, xa, type = c("prior", "matrix"), k = 5, ...){
                             v.names = "y",
                             varying = 2:ncol(draws),
                             times = names(draws)[2:ncol(draws)],
-                            timevar = "sample_ind")
+                            timevar = "draw")
 
     # Draw plot
 
-    p <- ggplot2::ggplot(data = draws, ggplot2::aes(x = x, y = y, colour = sample_ind)) +
+    p <- ggplot2::ggplot(data = draws, ggplot2::aes(x = x, y = y, colour = draw)) +
       ggplot2::geom_line(linewidth = 0.7) +
       ggplot2::labs(x = "x",
                     y = "y = f(x)",
@@ -86,56 +86,76 @@ plot.GPCov <- function(x, xa, type = c("prior", "matrix"), k = 5, ...){
 #'
 #' @importFrom stats qnorm median
 #' @param x \code{TSGP} object containing the model
-#' @param
+#' @param prob \code{numeric} scalar denoting the probability for the credible interval. Defaults to \code{0.95} for 95% interval
 #' @param draws \code{integer} denoting the number of draws to take from the posterior. Defaults to \code{100}
-#' @param robust \code{Boolean} denoting whether to use the mean (\code{FALSE}) or median (\code{FALSE}) to summarise the distribution. Defaults to \code{FALSE} for the mean
 #' @param ... arguments to be passed to methods
 #' @return \code{ggplot} object containing the plot
+#' @author Trent Henderson
+#' @export
+#' @examples
+#' #' x1 <- 1:100
+#' y <- 3 * sin(2 * seq(0, 4 * pi, length.out = 100)) + runif(100) * 2 + (0.08 * seq(from = 1, to = 100, by = 1))
+#'
+#' CovSum <- function(xa, xb, sigma_1 = 1, sigma_2 = 1, l_1 = 1, l_2 = 1, p = 1){
+#'   Sigma_exp_quad <- cov_exp_quad(xa, xb, sigma_1, l_1)
+#'   Sigma_periodic <- cov_periodic(xa, xb, sigma_2, l_2, p)
+#'   Sigma_noise <- cov_noise(sigma_3, nrow(Sigma_exp_quad))
+#'   Sigma <- Sigma_exp_quad + Sigma_periodic
+#'   return(Sigma)
+#' }
+#'
+#' mod <- GP(x1, 1:length(y), y, CovSum, 0.5)
+#' plot(mod, 0.95, 100)
 #'
 
-plot.TSGP <- function(x, conf = 0.95, draws = 100, robust = FALSE, ...){
+plot.TSGP <- function(x, prob = 0.95, draws = 100, ...){
 
   stopifnot(inherits(x, "TSGP") == TRUE)
-  stopifnot(conf > 0 & conf < 1)
+  stopifnot(prob > 0 & prob < 1)
 
   # Calculate z-score for Gaussian interval
 
-  half <- (1 - conf) / 2
+  half <- (1 - prob) / 2
   z <- stats::qnorm(half, lower.tail = FALSE)
 
   # Generate dataframe of draws from posterior
 
-  thenames <- paste0("Sample ", 1:k)
+  thenames <- paste0("Sample ", 1:draws)
   posterior <- as.data.frame(t(MASS::mvrnorm(n = draws, mu = x$mu, Sigma = x$Sigma)))
   colnames(posterior) <- thenames
 
-  posterior <- posterior %>%
-    mutate(timepoint = x$xprime) %>%
-    pivot_longer(cols = 1:ncol(posterior), names_to = "sample", values_to = "y")
+  # Wrangle into long format
 
-  if(robust){
-    posterior_ct <- posterior %>%
-      reframe(mu = stats::median(y), .by = "timepoint")
-  } else{
-    posterior_ct <- posterior %>%
-      reframe(mu = mean(y), .by = "timepoint")
-  }
+  posterior <- stats::reshape(posterior,
+                              direction = "long",
+                              v.names = "y",
+                              varying = 1:ncol(posterior),
+                              times = names(posterior)[1:ncol(posterior)],
+                              timevar = "draw")
 
-  posterior_sd <- data.frame(timepoint = x$xprime,
-                             sigma = sqrt(diag(x$Sigma))) # SD is square root of variance
+  # Calculate summaries
 
+  colnames(posterior) <- c("draw", "y", "timepoint")
+  rownames(posterior) <- NULL
+  posterior_ct <- aggregate(y ~ timepoint, data = posterior, FUN = function(x) mean(x, na.rm = TRUE))
+  colnames(posterior_ct) <- c("timepoint", "mu")
+  posterior_sd <- data.frame(timepoint = x$xprime, sigma = sqrt(diag(x$Sigma))) # SD is square root of variance
   posterior_summary <- merge(x = posterior_ct, y = posterior_sd, by = "timepoint")
   posterior_summary$lower <- posterior_summary$mu - (z * posterior_summary$sigma)
   posterior_summary$upper <- posterior_summary$mu + (z * posterior_summary$sigma)
+
+  # Get original data ready for plotting
+
+  original <- data.frame(timepoint = x$x, y = y)
 
   # Draw plot
 
   p <- ggplot2::ggplot(data = posterior_summary) +
     ggplot2::geom_ribbon(ggplot2::aes(x = timepoint, ymin = lower, ymax = upper), fill = "steelblue2", alpha = 0.5) +
-    ggplot2::geom_line(data = data, ggplot2::aes(x = timepoint, y = y), colour = "black") +
-    ggplot2::geom_point(data = data, ggplot2::aes(x = timepoint, y = y), colour = "black") +
+    ggplot2::geom_line(data = original, ggplot2::aes(x = timepoint, y = y), colour = "black") +
+    ggplot2::geom_point(data = original, ggplot2::aes(x = timepoint, y = y), colour = "black") +
     ggplot2::geom_line(ggplot2::aes(x = timepoint, y = mu), colour = "steelblue2", size = 1) +
-    ggplot2::labs(title = "Posterior ", ifelse(robust, "median", "mean"), " and ", round(conf * 100, digits = 0), "% prediction interval",
+    ggplot2::labs(title = paste0("Posterior mean and ", round(prob * 100, digits = 0), "% credible interval"),
                   subtitle = paste0("Interval calculated over ", draws, " samples"),
                   x = "Timepoint",
                   y = "Value") +
