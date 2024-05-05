ee0972cb984b77edadd2ea20484b81072c16a8a0
Trent Henderson <trent.henderson1@outlook.com>
2024-05-05 20:28:28 +1000

    started optim work

--- R/GP.R
+++ R/GP.R
@@ -1,10 +1,13 @@
 #' Calculate posterior mean vector and covariance matrix
 #'
+#' @importFrom stats optim
 #' @param x \code{numeric} vector of input data
 #' @param xprime \code{numeric} vector of data points to predict
 #' @param y \code{numeric} vector of values to learn from
 #' @param covfun \code{function} specifying the covariance function to use
 #' @param epsilon \code{numeric} scalar denoting a small quantity to add to the (x, x) covariance matrix for numerical stability. Defaults to \code{1e-9}
+#' @param optim \code{Boolean} whether to optimise hyperparameters for covariance functions using negative log likelihood. Defaults to \code{FALSE}
+#' @param method \code{character} denoting the optimisation algorithm in \code{stats::optim} to use if \code{optim = TRUE}. Defaults to \code{"L-BFGS-B"}
 #' @param ... covariance function hyperparameters to be passed to the covariance functions within \code{cov_function}
 #' @return \code{TSGP} object containing the input data, posterior mean function and covariance matrix
 #' @author Trent Henderson
@@ -27,15 +30,38 @@
 #'           l_1 = 75, l_2 = 1, p = 25)
 #'
 
-GP <- function(x, xprime, y, covfun, epsilon = 1e-9, ...){
-  Sigma_11 <- covfun(x, x, ...) + diag(epsilon, length(x)) # Add small diagonal epsilon for numerical stability
-  Sigma_12 <- covfun(x, xprime, ...)
-  Sigma_inv <- t(solve(Sigma_11, Sigma_12))
-  Sigma_22 <- covfun(xprime, xprime, ...)
-  Sigma_2 <- Sigma_22 - (Sigma_inv %*% Sigma_12) # Posterior covariance matrix
-  mu_2 <- Sigma_inv %*% y # Posterior mean vector
-  gp <- list(x, xprime, y, mu_2, Sigma_2)
-  names(gp) <- c("x", "xprime", "y", "mu", "Sigma")
+GP <- function(x, xprime, y, covfun, epsilon = 1e-9, optim = FALSE,
+               method = c("L-BFGS-B", "Nelder-Mead", "BFGS", "CG", "SANN", "Brent"), ...){
+
+  if(optim){
+
+    # Run optimisation procedure
+    #
+    # method <- match.arg(method)
+    #
+    # opt_result <- optim(par = initial_params, fn = neg_log_likelihood, x = x,
+    #                     xprime = xprime, y = y, covfun = covfun, epsilon = epsilon, method)
+    #
+    # optimized_params <- opt_result$par
+    #
+    # Compute final results using optimised parameters
+    #
+    #
+
+    stop("optimisation procedure is not currently implemented")
+
+  } else{
+
+    Sigma_11 <- covfun(x, x, ...) + diag(epsilon, length(x)) # Add small diagonal epsilon for numerical stability
+    Sigma_12 <- covfun(x, xprime, ...)
+    Sigma_inv <- t(solve(Sigma_11, Sigma_12))
+    Sigma_22 <- covfun(xprime, xprime, ...)
+    Sigma_2 <- Sigma_22 - (Sigma_inv %*% Sigma_12) # Posterior covariance matrix
+    mu_2 <- Sigma_inv %*% y # Posterior mean vector
+    gp <- list(x, xprime, y, mu_2, Sigma_2)
+    names(gp) <- c("x", "xprime", "y", "mu", "Sigma")
+  }
+
   gp <- structure(gp, class = c("TSGP", "list"))
   return(gp)
 }
---
+++ R/neg_log_likelihood.R
@@ -0,0 +1,32 @@
+#' Calculate negative log likelihood of a Gaussian process regression model
+#'
+#' @param params \code{vector} of hyperparameters
+#' @param x \code{numeric} vector of input data
+#' @param xprime \code{numeric} vector of data points to predict
+#' @param y \code{numeric} vector of values to learn from
+#' @param covfun \code{function} specifying the covariance function to use
+#' @param epsilon \code{numeric} scalar denoting a small quantity to add to the (x, x) covariance matrix for numerical stability. Defaults to \code{1e-9}
+#' @return \code{numeric} scalar of the negative log likelihood
+#' @author Trent Henderson
+#'
+
+neg_log_likelihood <- function(params, x, xprime, y, covfun, epsilon = 1e-9) {
+
+  # Calculate covariance matrices
+
+  Sigma_11 <- covfun(x, x, ...) + diag(epsilon, length(x)) # Add small diagonal epsilon for numerical stability
+  Sigma_12 <- covfun(x, xprime, ...)
+  Sigma_inv <- t(solve(Sigma_11, Sigma_12))
+  Sigma_22 <- covfun(xprime, xprime, ...)
+  Sigma_2 <- Sigma_22 - (Sigma_inv %*% Sigma_12) # Posterior covariance matrix
+  mu_2 <- Sigma_inv %*% y # Posterior mean vector
+
+  # Calculate log determinant
+
+  log_det <- sum(log(diag(chol(Sigma_11))))
+
+  # Calculate negative marginal log likelihood
+
+  nll <- 0.5 * (t(y) %*% solve(Sigma_11) %*% y + log_det + length(x) * log(2 * pi))
+  return(nll)
+}
